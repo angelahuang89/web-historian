8cd25c5bc2766a4d03c12b56ea5ab2c9d7e49155
angelahuang89 <angelahuang89@gmail.com>
2017-11-02 15:03:26 -0700

    Completed all archive helper functions

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var http = require('http');
+var request = require('request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -40,9 +42,9 @@ exports.readListOfUrls = function(callback) {
 
 exports.isUrlInList = function(url, callback) {
   exports.readListOfUrls(function(data) {
-    console.log('this is url: ', url);
-    console.log('this is data: ', data);
-    callback(data, url);
+    // console.log('this is url: ', url);
+    // console.log('this is data: ', data);
+    callback(data.includes(url));
   });
 //call this function first after given a POSTed url
   
@@ -80,27 +82,44 @@ exports.isUrlInList = function(url, callback) {
 
 exports.addUrlToList = function(url, callback) {
 
-  fs.appendFile(exports.paths.list, url, function(err, data){
+  fs.appendFile(exports.paths.list, url, function(err, data) {
     if (err) {
       throw err;
     }
-    
+    console.log('url: ', url);
+    console.log('list? ', exports.paths.list);
+    callback();
   });
-  //get sites.txt
-  //call appendFile
-    //add url to sites.txt
-  //call cb?
+    
 };
 
 exports.isUrlArchived = function(url, callback) {
-
   //returns true or false
-
-  //if true 
+  callback(fs.existsSync(this.paths.archivedSites + '/' + url));
+  
 };
 
 exports.downloadUrls = function(urls) {
+  console.log('these are our urls: ', urls);
+  console.log('paths/archivedSites: ', this.paths.archivedSites);
+  
+  //SEE TESTING.JS FILE TAB ABOVE
 
-  //use this to build a queue of urls for the worker to download
-  //worker will use this callback every 60 seconds
+  // request('http://www.google.com', function (error, response, body) {
+  //   console.log('error:', error); // Print the error if one occurred
+  //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //   console.log('body:', body); // Print the HTML for the Google homepage.
+  // });
+
+  urls.forEach((url) => {
+    request(url, (err, response, body) => {
+      fs.writeFile(this.paths.archivedSites + '/' + url, body, 'utf8', function(err) {
+        if (err) {
+          throw err;
+        }
+      });
+    });
+  });
+
+
 };
--- helpers/testing.js
+++ helpers/testing.js
@@ -1,1 +1,43 @@
-console.log(__dirname);
+var request = require('request');
+
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  // console.log('statusCode:', response ); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+// var http = require('http');
+
+// http.get('http://www.google.com', (res) => {
+//   const { statusCode } = res;
+//   const contentType = 'text/html';//res.headers['content-type'];
+
+//   let error;
+//   if (statusCode !== 200) {
+//     error = new Error('Request Failed.\n' +
+//                       `Status Code: ${statusCode}`);
+//   } else if (!/^text\/html/.test(contentType)) {
+//     error = new Error('Invalid content-type.\n' +
+//                       `Expected application/json but received ${contentType}`);
+//   }
+//   if (error) {
+//     console.error(error.message);
+//     // consume response data to free up memory
+//     res.resume();
+//     return;
+//   }
+
+//   res.setEncoding('utf8');
+//   let rawData = '';
+//   res.on('data', (chunk) => { rawData += chunk; });
+//   res.on('end', () => {
+//     try {
+//       const parsedData = rawData;
+//       console.log(parsedData);
+//     } catch (e) {
+//       console.error(e.message);
+//     }
+//   });
+// }).on('error', (e) => {
+//   console.error(`Got error: ${e.message}`);
+// });
